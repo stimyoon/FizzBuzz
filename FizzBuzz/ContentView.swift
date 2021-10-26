a0f1509dd4165a86fdfdee073f43223b4684201c
Tim Yoon <stimyoon@gmail.com>
2021-10-25 21:00:25 -0400

    Initial Commit

---
+++ FizzBuzz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FizzBuzz
+//
+//  Created by Tim Yoon on 10/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Tim Yoon on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
